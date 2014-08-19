55769d6c15fdbe4f3106f3d0488dd321d33196c0
jiunjiun <quietmes@gmail.com>
2014-08-20 01:25:36 +0800

    update setting

--- install-railsapp.sh
+++ install-railsapp.sh
@@ -18,6 +18,8 @@ echo -e "\e[31m[RailsAPP] Installing runtime js library for assets pipelie ...\e
 echo -e "\e[31m[RailsAPP] Update apt package list and install node.js ...\e[0m"
 sudo apt-get install nodejs
 sudo apt-get install build-essential libssl-dev
+# mysql lib client for gem => mysql2
+sudo apt-get install libmysqlclient-dev -y
 
 # 2. 安裝 rbenv
 echo -e "\e[31m[Rails] Installing RVM ...\e[0m"
@@ -31,25 +33,7 @@ git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-buil
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
 exec $SHELL
 
-# ---------------------
-# Setting deploy user
-# ---------------------
+rbenv install $RUBY_VERSION
+rbenv global $RUBY_VERSION
 
-# 1. 新增 rails application deploy user
-echo -e "\e[31m[RailsAPP] Create deploy user = $DEPLOY_USER ...\e[0m"
-useradd $DEPLOY_USER -m -G $WEB_GROUP -s /bin/bash
-echo -e "\e[31m[RailsAPP] Setting Deploy user password ...\e[0m"
-passwd $DEPLOY_USER
-
-# 2. 授權 public key 登入 deploy user
-mkdir /home/$DEPLOY_USER/.ssh/
-cp ~/.ssh/authorized_keys /home/$DEPLOY_USER/.ssh/authorized_keys
-
-# 3. 加入 sudoer 要把 deploy user 加到 web group，因為 static file 也需要 nginx 也需要有權限存取
-echo -e "\e[31m[RailsAPP] Add deploy user to sudoer list ...\e[0m"
-echo "$DEPLOY_USER  ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
-# 4. Setting deploy folder
-echo -e "\e[31m[Rails] Setting deploy user bash config ...\e[0m"
-mkdir /var/www
-chown $DEPLOY_USER:$DEPLOY_USER /var/www
+gem install rails
