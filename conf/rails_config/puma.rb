c3fab66b5e5b790eccd849f1eef827290767bfc9
jiunjiun <quietmes@gmail.com>
2014-08-31 01:45:39 +0800

    update puma setting

--- conf/rails_config/puma.rb
+++ conf/rails_config/puma.rb
@@ -11,6 +11,7 @@ pidfile "#{application_path}/shared/tmp/pids/puma.pid"
 state_path "#{application_path}/shared/tmp/sockets/puma.state"
 stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
 bind "unix://#{application_path}/shared/tmp/sockets/.sock"
+activate_control_app "unix://#{application_path}/shared/tmp/sockets/pumactl.sock"
 
 daemonize true
 on_restart do
